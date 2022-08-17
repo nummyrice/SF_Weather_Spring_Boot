20ff50eff10ee9abc064f828dc69c384a46fabf1
Nicholas Rice <nicholasroyrice@gmail.com>
2022-08-16 23:50:28 -0700

    server running

---
+++ src/test/java/tripActions_code_challenge/SFWeather/SfWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package tripActions_code_challenge.SFWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
